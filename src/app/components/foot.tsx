eabc2fa82b68d756888c3f9bea3aab8a0aab73af
Wei-H0ng <weihongchew2@gmail.com>
2023-09-30 21:24:42 +0800

    added footer

--- src/app/components/foot.tsx
+++ src/app/components/foot.tsx
@@ -3,18 +3,10 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer>
-            <div className="flex flex-col items-center justify-center sm:flex-row bg-sec">
-                <div>
-                    Linked-in
-                </div>
-                <div>
-                    github
-
-                </div>
-                <div>
-                    instagram
-
-                </div>
+            <div className="flex flex-col items-center justify-center md:flex-row bg-sec gap-4 h-28">
+                <a className="block w-16 h-3 text-pri">Linked-in</a>
+                <a className="block w-16 h-3 text-pri">Resume</a>
+                <a className="block w-16 h-3 text-pri">Git-Hub</a>
             </div>
         </footer>
     );
