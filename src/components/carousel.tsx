389562d3d84ab4c8a5688d3b1db0b76e8ae3d0eb
Wei-H0ng <weihongchew2@gmail.com>
2023-10-02 16:55:37 +0800

    utilise npm instead of web element for project section

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -7,18 +7,49 @@ import car from "public/tomica.jpg"
 import fish from "public/fish.jpg"
 import burger from "public/burger.jpg"
 
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import { Navigation, Pagination} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import {Pagination} from 'swiper/modules';
 
 
 export default function Carousel() {
 
     return (
         <div className=" w-full h-full">
-            <Script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></Script>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={true}
+                modules={[Pagination]}
+                >
+                <SwiperSlide>
+                    <Card 
+                        image={bean}
+                        altTxt = "a single bean"
+                        desc = "an image of fish's bean, i like rubbing it"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Card 
+                        image={fish}
+                        altTxt = "fish"
+                        desc = "my favourite fish"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Card 
+                       image={car}
+                        altTxt = "tomica toy car"
+                        desc = "Car that fish got me "/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Card 
+                        image={burger}
+                        altTxt = "McD's scrambled egg burger"
+                        desc = "A very dissapointing scrambled egg burger from McDonalds , egg was not scrambeled. Made me big sad"/>
+               
+                </SwiperSlide>
+            </Swiper>
+            {/* <Script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></Script>
             <swiper-container space-between="30" pagination="true" pagination-clickable="true">
                 <swiper-slide>
                     <Card 
@@ -44,7 +75,7 @@ export default function Carousel() {
                         altTxt = "McD's scrambled egg burger"
                         desc = "A very dissapointing scrambled egg burger from McDonalds , egg was not scrambeled. Made me big sad"/>
                 </swiper-slide>
-            </swiper-container>
+            </swiper-container> */}
         </div>
     )
 }
